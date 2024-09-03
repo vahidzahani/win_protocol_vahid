5ddbe46161516eca335aaf497665089ed5ac305c
vahid in F-org <you@example.com>
2024-09-03 10:08:49 +0330

    Add project files.

---
+++ win_protocol_vahid/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace win_protocol_vahid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(78, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 103);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ثبت در رجیستری";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Courier New", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(66, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 28);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "ftvahidpath://D:\\";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 267);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "ftvahidpath";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ win_protocol_vahid/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace win_protocol_vahid
+{
+    public partial class Form1 : Form
+    {
+        private string[] commandLineArgs;
+
+        public Form1(string[] args)
+        {
+            InitializeComponent();
+            commandLineArgs = args;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // نمایش آرگومان‌های ورودی
+            string argsMessage = "آرگومان‌های ورودی:\n";
+            foreach (string arg in commandLineArgs)
+            {
+                argsMessage += arg + "\n";
+            }
+            //MessageBox.Show(argsMessage, "آرگومان‌ها", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // بررسی و باز کردن مسیر
+            if (commandLineArgs.Length > 0)
+            {
+                string arg = commandLineArgs[0];
+                string path = ExtractPathFromArgument(arg);
+                //MessageBox.Show(path);
+
+                if (!string.IsNullOrEmpty(path))
+                {
+                    try
+                    {
+                        // بررسی اینکه آیا مسیر یک فایل یا پوشه معتبر است
+                        if (File.Exists(path) || Directory.Exists(path))
+                        {
+                            Process.Start("explorer.exe", path);
+                        }
+                        else
+                        {
+                            MessageBox.Show("مسیر مشخص شده وجود ندارد یا معتبر نیست.", "مسیر نامعتبر", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("خطا در باز کردن مسیر: " + ex.Message, "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                Application.Exit();
+            }
+        }
+
+        private string ExtractPathFromArgument(string arg)
+        {
+            // بررسی و استخراج مسیر از آرگومان
+            const string protocol = "ftvahidpath:";
+            if (arg.StartsWith(protocol, StringComparison.OrdinalIgnoreCase))
+            {
+                string encodedPath = arg.Substring(protocol.Length);
+                // رمزگشایی URL
+                string decodedPath = Uri.UnescapeDataString(encodedPath);
+                return decodedPath;
+            }
+            return string.Empty;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // مسیر فایل اجرایی برنامه
+                string exePath = Application.ExecutablePath;
+
+                // کلید پروتکل در رجیستری
+                string protocolKey = @"HKEY_CLASSES_ROOT\ftvahidpath";
+
+                // ایجاد کلید پروتکل
+                Microsoft.Win32.Registry.SetValue(protocolKey, "", "URL:ftvahidpath Protocol");
+                Microsoft.Win32.Registry.SetValue(protocolKey, "URL Protocol", "");
+
+                // ایجاد کلید SubKey برای دستورات
+                string commandKey = @"HKEY_CLASSES_ROOT\ftvahidpath\shell\open\command";
+                Microsoft.Win32.Registry.SetValue(commandKey, "", "\"" + exePath + "\" \"%1\"");
+
+                // نمایش پیام موفقیت
+                MessageBox.Show("پروتکل 'ftvahidpath' با موفقیت ثبت شد.", "موفقیت", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                // نمایش پیام خطا
+                MessageBox.Show("خطا در ثبت پروتکل: " + ex.Message, "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
